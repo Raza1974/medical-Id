8fca2e159eff3a56136fccb17654e4904d2f3ac9
Asim74 <asimr6573@gmail.com>
2024-10-17 01:06:50 +0500

    first commit

--- src/app/health-blogs/page.tsx
+++ src/app/health-blogs/page.tsx
@@ -1,123 +1,78 @@
 import Image from "next/image";
 import React from "react";
 
+// Define the type for each blog entry
+type IData = {
+    title: string;
+    description: string;
+    image: string;
+};
+
+// Sample data: an array of blog entries
+const data: IData[] = [
+    {
+        title: "Fitness",
+        description: "Fitness and Exercise: Health Benefits, How to Get Started.",
+        image: "/image/fitness.png",
+    },
+    {
+        title: "Heart Health",
+        description: "You can help prevent heart disease by following a heart-healthy lifestyle.",
+        image: "/image/heart health.jpg",
+    },
+    {
+        title: "Diabetes",
+        description: "Lose extra weight. Losing weight reduces the risk of diabetes.",
+        image: "/image/diabetes.jpg",
+    },
+    {
+        title: "Heart Disease",
+        description: "An enlarged heart means you have a condition that has increased the size of your heart.",
+        image: "/image/heart disease.jpg",
+    },
+    {
+        title: "Dengue Prevention",
+        description: "An enlarged heart means you have a condition that has increased the size of your heart.",
+        image: "/image/dengue.jpg",
+    },
+    {
+        title: "chikungunya",
+        description: "An enlarged heart means you have a condition that has increased the size of your heart.",
+        image: "/image/chikungunya.jpg",
+    },
+    {
+        title: "Diarrhea",
+        description: "An enlarged heart means you have a condition that has increased the size of your heart.",
+        image: "/image/diarrhea.jpg",
+    },
+    {
+        title: "Malaria",
+        description: "An enlarged heart means you have a condition that has increased the size of your heart.",
+        image: "/image/malaria.jpg",
+    },
+    {
+        title: "Typhiod",
+        description: "An enlarged heart means you have a condition that has increased the size of your heart.",
+        image: "/image/typhiod.jpg",
+    },
+];
+
 export default function HealthBlog() {
     return (
         <div className="parentContainer">
-            {/* Fitness Section */}
-            <div className="childContainer bounce-in-right">
-                <div className="imageContainer">
-                    <Image
-                        src="/image/fitness.png"
-                        alt="Health Fitness"
-                        width={600} // Specify the width
-                        height={400} // Specify the height
-                        className="object-cover w-full h-30 sm:h-48 md:h-52 lg:h-56"
-                    />
-                </div>
-                <h2 className="title">Fitness</h2>
-                <p className="description">
-                    Fitness and Exercise: Health Benefits, How to Get Started.
-                </p>
-                <a href="https://www.everydayhealth.com/fitness/guide/" className="readMoreLink">
-                    <button className="readMore">Read More</button>
-                </a>
-            </div>
-
-            {/* Heart Health Section */}
-            <div className="childContainer bounce-in-right">
-                <div className="imageContainer">
-                    <Image
-                        src="/image/heart health.jpg"
-                        alt="Heart Health"
-                        width={600} // Specify the width
-                        height={400} // Specify the height
-                        className="object-cover w-full h-30 sm:h-48 md:h-52 lg:h-56"
-                    />
-                </div>
-                <h2 className="title">Heart Health</h2>
-                <p className="description">
-                    You can help prevent heart disease by following a heart-healthy lifestyle.
-                </p>
-                <a href="https://www.healthline.com/nutrition/heart-healthy-foods" className="readMoreLink">
-                    <button className="readMore">Read More</button>
-                </a>
-            </div>
-
-            {/* Diabetes Section */}
-            <div className="childContainer bounce-in-right">
-                <div className="imageContainer">
-                    <Image
-                        src="/image/diabetes.jpg"
-                        alt="Diabetes Care"
-                        width={600} // Specify the width
-                        height={400} // Specify the height
-                        className="object-cover w-full h-30 sm:h-48 md:h-52 lg:h-56"
-                    />
-                </div>
-                <h2 className="title">Diabetes</h2>
-                <p className="description">
-                    Lose extra weight. Losing weight reduces the risk of diabetes.
-                </p>
-                <a href="https://diabetesjournals.org/care" className="readMoreLink">
-                    <button className="readMore">Read More</button>
-                </a>
-            </div>
-
-            {/* Heart Disease Section */}
-            <div className="childContainer bounce-in-right">
-                <div className="imageContainer">
-                    <Image
-                        src="/image/heart disease.jpg"
-                        alt="Heart Disease"
-                        width={600} // Specify the width
-                        height={400} // Specify the height
-                        className="object-cover w-full h-30 sm:h-48 md:h-52 lg:h-56"
-                    />
-                </div>
-                <h2 className="title">Heart Disease</h2>
-                <p className="description">
-                    An enlarged heart means you have a condition that has increased the size of your heart.
-                </p>
-                <a href="https://www.mayoclinic.org/diseases-conditions/heart-disease/symptoms-causes/causes/2008195" className="readMoreLink">
-                    <button className="readMore">Read More</button>
-                </a>
-            </div>
-
-            {/* Additional Blog Titles */}
-            <div className="childContainer bounce-in-right">
-                <div className="imageContainer">
-                    <Image
-                        src="/image/heart disease.jpg"
-                        alt="Heart Disease"
-                        width={600} // Specify the width
-                        height={400} // Specify the height
-                        className="object-cover w-full h-30 sm:h-48 md:h-52 lg:h-56"
-                    />
-                </div>
-                <h2 className="title">Blog Title</h2>
-                <p className="description">
-                    Example blog content for this section.
-                </p>
-                <button className="readMore">Read More</button>
-            </div>
-
-            {/* Repeated Blog Titles */}
-            {[...Array(3)].map((_, index) => (
-                <div className="childContainer bounce-in-right" key={`blog-${index}`}>
+            {data.map((entry, index) => (
+                <div className="childContainer bounce-in-right bg-gradient-to-r from-green-400 via-blue-500 to-purple-600" key={index}>
                     <div className="imageContainer">
                         <Image
-                            src="/image/heart disease.jpg"
-                            alt="Heart Disease"
+                            src={entry.image}
+                            alt={entry.title}
                             width={600} // Specify the width
                             height={400} // Specify the height
                             className="object-cover w-full h-30 sm:h-48 md:h-52 lg:h-56"
                         />
                     </div>
-                    <h2 className="title">Blog Title</h2>
-                    <p className="description">
-                        Sample description text for another blog.
-                    </p>
+                    <h1 className="title">{entry.title}</h1>
+                    <p className="description">{entry.description}</p>
                     <button className="readMore">Read More</button>
                 </div>
             ))}
--- src/app/medical-info/page.tsx
+++ src/app/medical-info/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link'; // Ensure you're importing the Link component
 
 export default function Home() {
   return (
-    <main className="mb-4 text-center childContainerInformation bounce-in-right">
+    <main className="mb-4 text-center childContainerInformation bounce-in-right bg-gradient-to-r from-green-600 via-blue-500 to-purple-1000">
+      
       <div className="imageContainerInformation">
         <Image
           src="/image/medical branch.jpg"      
